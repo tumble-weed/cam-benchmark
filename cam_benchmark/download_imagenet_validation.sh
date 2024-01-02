2c1b051210c899e535d176d4deff73c7123ef4b5
Aniket Singh <aniketsinghresearch@gmail.com>
2024-01-02 16:16:02 +0000

    download imagenet

--- cam_benchmark/download_imagenet_validation.sh
+++ cam_benchmark/download_imagenet_validation.sh
@@ -1,10 +1,16 @@
-mkdir -p imagenet/bboxes
-gdown 173q2SclMLyVyZbIvGh_0d6P06gL7mMbt
-tar -xvzf ILSVRC2012_bbox_val_v3.tgz -C imagenet/bboxes
+dataset_dir="/root/bigfiles/datasets"
+(
+mkdir -p $dataset_dir/imagenet/bboxes  && \
+gdown 173q2SclMLyVyZbIvGh_0d6P06gL7mMbt && \
+tar -xvzf ILSVRC2012_bbox_val_v3.tgz -C $dataset_dir/imagenet/bboxes && \
+ #imagenet/bboxes
 #=================================================
-gdown 1-5IOWkSv0VwO9WoGKW0YYDfRjMnVVvPi
-mkdir -p imagenet/images
-unzip imagenet_val.zip -d imagenet/images
-mv imagenet/images/imagenet/* imagenet/images/
-rmdir imagenet/images/imagenet
+gdown 1-5IOWkSv0VwO9WoGKW0YYDfRjMnVVvPi && \
+mkdir -p $dataset_dir/imagenet/images && \
+unzip imagenet_val.zip -d $dataset_dir/imagenet/images && \
+mv $dataset_dir/imagenet/images/imagenet/* $dataset_dir/imagenet/images/ && \
+rmdir $dataset_dir/imagenet/images/imagenet &&
+rm imagenet_val.zip &&
+rm ILSVRC2012_bbox_val_v3.tgz
+)
 #mv imagenet
